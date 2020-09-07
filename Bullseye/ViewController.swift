d4343edfd60fb0efd45e734d0ea0429018a44f7b
Suchwinder Singh <windersingh80@yahoo.com>
2020-09-06 20:25:38 -0400

    Set up slider, button, working on target value

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentValue: Int = 0
+    var targetValue = 0
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        startNewRound()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentValue)" +
+                    "\nThe target value is: \(targetValue)"
+        
+        let alert = UIAlertController(title: "Hello, World!",
+                                      message: message,
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated:true, completion:nil)
+        
+        startNewRound()
+    }
+    
+    @IBAction func silderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
 }
 
