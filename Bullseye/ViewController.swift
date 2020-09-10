49ae9e9248a4ee9f614ad6a9e631e055792d2c89
Suchwinder Singh <windersingh80@yahoo.com>
2020-09-10 16:40:37 -0400

    Added conditionals to show how close urser was to scoring perfect score, and working on start over feature

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -26,21 +26,41 @@ class ViewController: UIViewController {
     
     @IBAction func showAlert() {
         let difference = abs(targetValue - currentValue)
-        let points = 100 - difference
+        var points = 100 - difference
+        
+        let title: String;
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it!"
+            if(difference == 1) {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
         score += points
+
         let message = "You scored \(points) points"
-        let alert = UIAlertController(title: "Hello, World!",
+        let alert = UIAlertController(title: title,
                                       message: message,
                                       preferredStyle: .alert)
-        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        let action = UIAlertAction(title: "Ok", style: .default, handler: {
+            _ in self.startNewRound()
+        })
         alert.addAction(action)
         present(alert, animated:true, completion:nil)
-        
-        startNewRound()
     }
     
     @IBAction func silderMoved(_ slider: UISlider) {
         currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func startOver() {
+        startNewGame()
     }
     
     func startNewRound() {
@@ -55,6 +75,12 @@ class ViewController: UIViewController {
         targetLabel.text = String(targetValue)
         scoreLabel.text = String(score)
         roundLabel.text = String(round)
+    }
+    
+    func startNewGame() {
+        score = 0
+        round = 0
+        startNewRound()
     }
 }
 
