3559c5c2c7f2d47d88c6ae7f45413da11628e176
Suchwinder Singh <windersingh80@yahoo.com>
2020-09-18 17:24:00 -0400

    created an array to store the data we will be using to display scores

---
+++ Bullseye/HighScoreItem.swift
@@ -0,0 +1,14 @@
+//
+//  HighScoreItem.swift
+//  Bullseye
+//
+//  Created by Suchwinder Singh on 9/18/20.
+//  Copyright © 2020 Suchwinder Singh. All rights reserved.
+//
+
+import Foundation
+
+class HighScoreItem {
+    var name = ""
+    var score = 0
+}
--- Bullseye/HighScoresViewController.swift
+++ Bullseye/HighScoresViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
-class HighScoresViewController: UITableViewController {
 
+class HighScoresViewController: UITableViewController {
+    var items = [HighScoreItem]()
+    var row0item = HighScoreItem()
+    var row1item = HighScoreItem()
+    var row2item = HighScoreItem()
+    var row3item = HighScoreItem()
+    var row4item = HighScoreItem()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let item1 = HighScoreItem()
+        item1.name = "The reader of this book"
+        item1.score = 50000
+        items.append(item1)
+        let item2 = HighScoreItem()
+        item2.name = "Manda"
+        item2.score = 10000
+        items.append(item2)
+        let item3 = HighScoreItem()
+        item3.name = "Joey"
+        item3.score = 5000
+        items.append(item3)
+        let item4 = HighScoreItem()
+        item4.name = "Adam"
+        item4.score = 1000
+        items.append(item4)
+        let item5 = HighScoreItem()
+        item5.name = "Eli"
+        item5.score = 500
+        items.append(item5)
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -29,32 +56,22 @@ class HighScoresViewController: UITableViewController {
 
     // MARK:- Table View Data Source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return items.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
     UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "HighScoreItem",
             for: indexPath)
+        
+        let item = items[indexPath.row]
+        
         let nameLabel = cell.viewWithTag(1000) as! UILabel
         let scoreLabel = cell.viewWithTag(2000) as! UILabel
         
-        if indexPath.row == 0 {
-            nameLabel.text = "The reader of this book"
-            scoreLabel.text = "50000"
-        } else if indexPath.row == 1 {
-            nameLabel.text = "Manda"
-            scoreLabel.text = "10000"
-        } else if indexPath.row == 2 {
-            nameLabel.text = "Joey"
-            scoreLabel.text = "5000"
-        } else if indexPath.row == 3 {
-            nameLabel.text = "Adam"
-            scoreLabel.text = "1000"
-        } else if indexPath.row == 4 {
-            nameLabel.text = "Eli"
-            scoreLabel.text = "500"
-        }
+        nameLabel.text = item.name
+        scoreLabel.text = String(item.score)
+        
         return cell
     }
     
