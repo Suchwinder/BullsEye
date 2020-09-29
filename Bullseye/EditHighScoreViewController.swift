729dc5ca3555fff1cc950b9275d395049c6a4649
Suchwinder Singh <windersingh80@yahoo.com>
2020-09-29 15:15:06 -0400

    ' Code Refasctoring and error in edit high score done button complete

--- Bullseye/EditHighScoreViewController.swift
+++ Bullseye/EditHighScoreViewController.swift
@@ -1,137 +1,58 @@
-//
-//  EditHighScoreViewController.swift
-//  Bullseye
-//
-//  Created by Suchwinder Singh on 9/19/20.
-//  Copyright © 2020 Suchwinder Singh. All rights reserved.
-//
-
 import UIKit
 
 protocol EditHighScoreViewControllerDelegate: class {
-    func editHighScoreViewControllerDidCancel(_ controller: EditHighScoreViewController)
-    func editHighScoreViewController(_ controller: EditHighScoreViewController,
-                                     didFinishEditing item: HighScoreItem)
+  func editHighScoreViewControllerDidCancel(
+    _ controller: EditHighScoreViewController)
+  func editHighScoreViewController(
+    _ controller: EditHighScoreViewController,
+    didFinishEditing item: HighScoreItem)
 }
 
 class EditHighScoreViewController: UITableViewController, UITextFieldDelegate {
-    weak var delegate: EditHighScoreViewControllerDelegate?
-    var highScoreItem: HighScoreItem!
+  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var doneBarButton: UIBarButtonItem!
+  weak var delegate: EditHighScoreViewControllerDelegate?
+  var highScoreItem: HighScoreItem!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    textField.text = highScoreItem.name
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    textField.becomeFirstResponder()
+  }
+  
+  // MARK:- Actions
+  @IBAction func cancel() {
+    delegate?.editHighScoreViewControllerDidCancel(self)
+  }
+
+  @IBAction func done() {
+    highScoreItem.name = textField.text!
+
+    delegate?.editHighScoreViewController(self, didFinishEditing: highScoreItem)
+  }
+  
+  // MARK:- Table View Delegates
+  override func tableView(_ tableView: UITableView,
+            willSelectRowAt indexPath: IndexPath)
+            -> IndexPath? {
+    return nil
+  }
+  
+  // MARK:- Text Field Delegates
+  func textField(_ textField: UITextField,
+                 shouldChangeCharactersIn range: NSRange,
+                 replacementString string: String) -> Bool {
+
+    let oldText = textField.text!
+    let stringRange = Range(range, in:oldText)!
+    let newText = oldText.replacingCharacters(in: stringRange,
+                                            with: string)
+    doneBarButton.isEnabled = !newText.isEmpty
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        textField.text = highScoreItem.name
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-    
-    //MARK:- Actions
-    @IBAction func cancel() {
-        delegate?.editHighScoreViewControllerDidCancel(self)
-    }
-    
-    @IBAction func done() {
-        highScoreItem.name = textField.text!
-        delegate?.editHighScoreViewController(self, didFinishEditing: highScoreItem)
-    }
-    
-    @IBOutlet weak var textField: UITextField!
-    
-    // MARK: - Table View Delegates
-    override func tableView(_ tableView: UITableView,
-                            willSelectRowAt indexPath: IndexPath) -> IndexPath? {
-        return nil
-    }
-    
-    
-    @IBOutlet weak var doneBarButton: UIBarButtonItem!
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        textField.becomeFirstResponder()
-    }
-
-    //MARK: - Text Field Delegates
-    func textField(_ textField: UITextField,
-                   shouldChangeCharactersIn range: NSRange,
-                   replacementString string: String) -> Bool {
-        let oldText = textField.text!
-        let stringRange = Range(range, in: oldText)!
-        let newText = oldText.replacingCharacters(in: stringRange, with: string)
-        
-        doneBarButton.isEnabled = !newText.isEmpty
-        
-        return true
-    }
-    
-    // MARK: - Table view data source
-
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return 0
-//    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    return true
+  }
 }
--- Bullseye/HighScoresViewController.swift
+++ Bullseye/HighScoresViewController.swift
@@ -8,46 +8,17 @@
 
 import UIKit
 
-
 class HighScoresViewController: UITableViewController, EditHighScoreViewControllerDelegate {
-    //MARK: - Edit High Score View Controller Delegates
-    func editHighScoreViewControllerDidCancel(_ controller: EditHighScoreViewController) {
-        navigationController?.popViewController(animated: true)
-    }
-    
-    func editHighScoreViewController(_ controller: EditHighScoreViewController, didFinishEditing item: HighScoreItem) {
-        if let index = items.firstIndex(of: item) {
-            let indexPath = IndexPath(row: index, section: 0)
-            let indexPaths = [indexPath]
-            tableView.reloadRows(at: indexPaths, with: .automatic)
-        }
-        PersistencyHelper.saveHighScores(items)
-        navigationController?.popViewController(animated:true)
-    }
-    
-    // MARK:- Navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let controller = segue.destination as! EditHighScoreViewController
-        controller.delegate = self
-        if let indexPath = tableView.indexPath(for: sender as! UITableViewCell) {
-            controller.highScoreItem = items[indexPath.row]
-        }
-    }
-    
     var items = [HighScoreItem]()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        items = PersistencyHelper.loadHighScores()
-        if(items.count == 0) {
-            resetHighScores()
-        }
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      items = PersistencyHelper.loadHighScores()
+      if (items.count == 0) {
+        resetHighScores()
+      }
     }
+    
     // MARK:- Actions
     @IBAction func resetHighScores() {
         items = [HighScoreItem]()
@@ -75,22 +46,17 @@ class HighScoresViewController: UITableViewController, EditHighScoreViewControll
         PersistencyHelper.saveHighScores(items)
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
     // MARK:- Table View Data Source
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+      return items.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
-    UITableViewCell {
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath)
+                            -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "HighScoreItem",
-            for: indexPath)
+                                                 for: indexPath)
         
         let item = items[indexPath.row]
         
@@ -109,7 +75,7 @@ class HighScoresViewController: UITableViewController, EditHighScoreViewControll
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
-    override func tableView(_ tavleView: UITableView,
+    override func tableView(_ tableView: UITableView,
                              commit editingStyle: UITableViewCell.EditingStyle,
                              forRowAt indexPath: IndexPath) {
         items.remove(at: indexPath.row)
@@ -118,60 +84,29 @@ class HighScoresViewController: UITableViewController, EditHighScoreViewControll
         tableView.deleteRows(at: indexPaths, with: .automatic)
         PersistencyHelper.saveHighScores(items)
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    //MARK: - Edit High Score View Controller Delegates
+    func editHighScoreViewControllerDidCancel(_ controller: EditHighScoreViewController) {
+        navigationController?.popViewController(animated: true)
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    func editHighScoreViewController(_ controller: EditHighScoreViewController,
+                                     didFinishEditing item: HighScoreItem) {
+        if let index = items.firstIndex(of: item) {
+            let indexPath = IndexPath(row: index, section: 0)
+            let indexPaths = [indexPath]
+            tableView.reloadRows(at: indexPaths, with: .automatic)
+        }
+        PersistencyHelper.saveHighScores(items)
+        navigationController?.popViewController(animated:true)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
+    // MARK:- Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let controller = segue.destination as! EditHighScoreViewController
+        controller.delegate = self
+        if let indexPath = tableView.indexPath(for: sender as! UITableViewCell) {
+            controller.highScoreItem = items[indexPath.row]
+        }
     }
-    */
-
 }
